3e13912d8211189aed89610a54d245210f5ca187
Shrayas <shrayasr@gmail.com>
2014-12-22 19:37:52 +0530

    Adding preprocessor for portability
    - use platform specific libs

--- repl.c
+++ repl.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#elif __APPLE__
 #include<editline/readline.h>
+
+#else
+#include<editline/readline.h>
+#include<editline/history.h>
+
+#endif
 
 int main(int argc, char **argv) {
 

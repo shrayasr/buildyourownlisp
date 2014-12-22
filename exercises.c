d0f15eeba1587fb9c52def8d879d33de042e3916
Shrayas <shrayasr@gmail.com>
2014-12-22 20:25:54 +0530

    Solving some exercises

---
+++ exercises.c
@@ -0,0 +1,18 @@
+// Chapter 5
+
+/*
+ * Write down some more examples of strings the Doge language contains.
+ *
+ * such lisp many language 
+ * such language many book so c
+ * wow lisp such build many book so language
+ */
+
+/*
+ * Why are there back slashes \ in front of the quote marks " in the grammar?
+ *
+ * \ are used as escape characters. Since the grammer defn starts with ", using
+ * another " will close it, so using \" signifies a literal double quote char
+ * in the grammar
+ */
+
